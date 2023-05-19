deb8b1b52d6301a2df80746ff6f0d657d09e82d7
asepmuliad <asepmuliad@gmail.com>
2023-05-19 07:21:54 +0700

    https://github.com/asepmuliad/QRcodeComponent.git

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/QRcodeComponen.vue'
+
+</script>
+
+<template>
+ 
+      <HelloWorld/>
+  <main>
+    
+  </main>
+</template>
+
+
---
+++ src/components/QRcodeComponen.vue
@@ -0,0 +1,20 @@
+<script setup>
+ 
+</script>
+
+<template>
+  <div class="card flex justify-center items-center h-screen bg-gray-200" id="card"> 
+         <div class="flex flex-col items-center bg-white max-w-sm rounded-lg overflow-hidden shadow-lg ">
+           <picture class="flex items-center justify-center p-3 ">
+             <img alt="QRcode" class="logo rounded-lg w-48 " src="../images/image-qr-code.png" />
+            </picture>
+               <h1 class="font-bold text-blue-950 text-center text-sm w-48 ">Improve your front-end skills by building projects </h1>
+                <text class="w-48 text-xs text-center pt-2 pb-3 lg:pb-6 text-gray-500 ">
+                  <p>Scan the QR code to visit Frontend</p>
+                     <p>Mentor and take your coding skills to the next level</p>
+                </text>
+        </div>
+  </div>
+</template>
+
+
