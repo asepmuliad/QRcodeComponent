6045fcdaef56e59c3e5c253c5b6bead28252222c
asepmuliad <asepmuliad@gmail.com>
2023-05-19 07:30:01 +0700

    Improve your front-end skills by building projects

--- src/components/QRcodeComponen.vue
+++ src/components/QRcodeComponen.vue
@@ -6,7 +6,7 @@
   <div class="card flex justify-center items-center h-screen bg-gray-200" id="card"> 
          <div class="flex flex-col items-center bg-white max-w-sm rounded-lg overflow-hidden shadow-lg ">
            <picture class="flex items-center justify-center p-3 ">
-             <img alt="QRcode" class="logo rounded-lg w-48 " src="../images/image-qr-code.png" />
+             <img alt="QRcode" class="logo rounded-lg w-48 " src="../assets/images/image-qr-code.png" />
             </picture>
                <h1 class="font-bold text-blue-950 text-center text-sm w-48 ">Improve your front-end skills by building projects </h1>
                 <text class="w-48 text-xs text-center pt-2 pb-3 lg:pb-6 text-gray-500 ">
