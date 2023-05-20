4e695484b31ff31ffea8e541889914352e0be7e2
asepmuliad <asepmuliad@gmail.com>
2023-05-20 08:29:09 +0700

    siap

--- src/components/AdviceGenerator.vue
+++ src/components/AdviceGenerator.vue
@@ -30,16 +30,16 @@ export default{
 </script>
 <template>
     <div class="card flex justify-center items-center h-screen bg-bgfull" id="card"> 
-         <div class="flex flex-col items-center bg-card max-w-sm rounded-lg shadow-lg relative" v-for="isi in cardData">
-               <p class=" text-cardl text-center text-xs w-48 pt-6 ">ADVICE #{{isi.id}} </p>
-                <text class="w-64 md:text-lg font-bold text-center pt-2 pb-3 lg:pb-9 lg:px-6 text-cardp ">
+         <div class="flex flex-col items-center bg-card max-w-sm rounded-lg shadow-lg shadow-cardbut relative" v-for="isi in cardData">
+               <p class=" text-cardl text-center text-xs w-48 pt-6 tracking-[.205em]">ADVICE #{{isi.id}} </p>
+                <text class="w-64 md:text-lg font-bold text-center pt-2 pb-3 lg:pb-9 lg:px-6 text-cardp leading-relaxed">
                   <h1>"{{isi.advice}}"</h1>
                 </text>
                 <img src="../assets/images/pattern-divider-desktop.svg" alt="" class="hidden sm:block pb-12" width="240">
                 <img src="../assets/images/pattern-divider-mobile.svg" alt="" class="pb-12 sm:hidden"  width="240">
                 <div class="max-w-lg bg-cardbut rounded-full absolute -bottom-5">
                            <button class="sm:p-3 p-2" @click="loadData">
-                        <img src="../assets/images/icon-dice.svg" alt="">
+                        <img src="../assets/images/icon-dice.svg" alt="" class="">
                            </button>
                 </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,19 @@ module.exports = {
     "./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      'card': '#323A49',
-      'cardl': '#ffe474',
-      'bgfull': '#1F2632',
-      'cardp': '#CEE3E9',
-      'cardbut': '#52FFA8',
-     
+    extend: {
+      colors: {
+        'card': '#323A49',
+        'cardl': '#ffe474',
+        'bgfull': '#1F2632',
+        'cardp': '#CEE3E9',
+        'cardbut': '#52FFA8',
+      },
+      fontFamily: {
+        'sans': ["Manrope"],
+      }
     },
+    
   },
 
   plugins: [],
